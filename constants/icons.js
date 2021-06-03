86e6c9161c6d684358c1e050aff041c2fc3ca5a1
vali <v.naeimabadi@gmail.com>
2021-06-03 20:59:36 +0430

    add cover images

--- constants/icons.js
+++ constants/icons.js
@@ -5,6 +5,12 @@ const back_icon = require('../assets/icons/back_icon.png');
 const sort_icon = require('../assets/icons/sort_icon.png');
 const image_picker = require('../assets/icons/image_picker.png');
 const close = require('../assets/icons/close.png');
+const cover_orang = require('../assets/icons/cover_orang.png');
+const cover_red = require('../assets/icons/cover_red.png');
+const cover_blue = require('../assets/icons/cover_blue.png');
+const cover_yellow = require('../assets/icons/cover_yellow.png');
+const cover_green = require('../assets/icons/cover_green.png');
+const cover_pink = require('../assets/icons/cover_pink.png');
 
 export default {
   logo,
@@ -13,5 +19,11 @@ export default {
   delete_icon,
   back_icon,
   image_picker,
-  close
+  close,
+  cover_orang,
+  cover_red,
+  cover_blue,
+  cover_yellow,
+  cover_green,
+  cover_pink
 };
